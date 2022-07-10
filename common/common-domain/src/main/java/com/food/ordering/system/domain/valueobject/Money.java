664e720384ad79e94c526ee8e00f2e3888fd3d82
sera <cieroyou1@gmail.com>
2022-07-10 21:02:06 +0900

    common-domain 모듈 추가, entity, valueobject, event 정의 및 추가

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,62 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return
+     */
+    public boolean isGraterThanZero() {
+        //equals 를 사용하면, 0.00과 같은 소수점이 있는 큰 소수가 있고 0을 사용하는 경우
+        //정수를 비교하면 올바른 결과를 반환하지 않으므로 compareTo 사용
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGraterThan(Money money) {
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money) {
+        return new Money(setScale(this.amount.add(money.getAmount())));
+    }
+
+    public Money substract(Money money) {
+        return new Money(setScale(this.amount.subtract(money.getAmount())));
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    /**
+     * 소수점 두자리로 만들어준다. ex) 10.05, 500.80
+     */
+    private BigDecimal setScale(BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderStatus.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.domain.valueobject;
+
+public enum OrderStatus {
+    PENDING,
+
+    PAID,
+
+    APPROVED,
+
+    CANCELLING,
+
+    CANCLLED;
+}
